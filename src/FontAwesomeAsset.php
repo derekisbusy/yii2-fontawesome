dee2e62583afe99aaf1040108a3c08307b670e41
Derek Gifford <derekisbusy@gmail.com>
2019-06-28 15:21:44 -0400

    Updates

--- src/FontAwesomeAsset.php
+++ src/FontAwesomeAsset.php
@@ -5,13 +5,13 @@ use yii\web\AssetBundle;
 
 class FontAwesomeAsset extends AssetBundle
 {
-    public $sourcePath = '@vendor/derekisbusy/yii2-fontawesome/src';
+    public $sourcePath = '@derekisbusy/fa/assets';
     
     public $css = [
         YII_ENV_DEV ? 'css/fontawesome.css' : 'css/fontawesome.min.css',
     ];
     
     public $js = [
-        'js/fa.min.js',
+//        'js/fa.min.js',
     ];
 }
